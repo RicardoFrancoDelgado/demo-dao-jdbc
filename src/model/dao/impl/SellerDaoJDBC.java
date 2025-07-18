498167076cbb1a6771fd0f3d5b899be87051582d
RicardoFrancoDelgado <rf041555@gmail.com>
2025-07-18 20:31:25 -0300

    change: FindById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,22 @@
 package model.dao.impl;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection conn;
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller obj) {
 
@@ -23,7 +34,42 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+              "SELECT seller.*,department.Name as DepName\n" +
+                      "FROM seller INNER JOIN department\n" +
+                      "ON seller.DepartmentId = department.Id\n" +
+                      "WHERE seller.Id = ?"
+            );
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                // comecamos instanciando um departamento e pegando os valores por nome das colunas no banco de dados
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                // vamos agora instanciar um seller com o mesmo método e passando um departamento associado no final
+                Seller seller = new Seller();
+                seller.setId(rs.getInt("Id"));
+                seller.setName(rs.getString("Name"));
+                seller.setEmail(rs.getString("Email"));
+                seller.setBirthDate(rs.getDate("BirthDate"));
+                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                seller.setDepartment(dep);
+                return seller;
+            }
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
     }
 
     @Override
